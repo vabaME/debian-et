cf928689fb84c750808e01d78f2892f50bcbce51
vaba <92581921+vabaME@users.noreply.github.com>
2023-06-26 15:56:06 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#coreutils
+
+sudo install -Dm644  ufw.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  gufw.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  synaptic.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  gnome-disk-utility.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  psensor.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  shadow.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  sudo.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  sudoers.mo /usr/share/locale/et/LC_MESSAGES/
+exit 1
