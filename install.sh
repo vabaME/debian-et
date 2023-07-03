7469eae733814450180a71a9ffde50cf2e772496
vaba <92581921+vabaME@users.noreply.github.com>
2023-07-03 11:44:21 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ sudo install -Dm644  psensor.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  shadow.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  sudo.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  sudoers.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  nano.mo /usr/share/locale/et/LC_MESSAGES/
 exit 1
